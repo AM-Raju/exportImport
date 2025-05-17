c6739ad1f32c3e60f086c657cff8adcfffc83c9c
AM-Raju <mdmorshed.sj2005@gmail.com>
2025-05-17 16:59:07 +0600

    Why Us page creation done

--- src/app/(homelayout)/profile/whyUs/page.jsx
+++ src/app/(homelayout)/profile/whyUs/page.jsx
@@ -32,16 +32,28 @@ const data = [
       "Innovation is at the core of what we do. We continuously stay ahead of industry advancements, leveraging modern technologies and forward-thinking strategies to enhance safety, efficiency, and operational performance. Our solutions are designed to not only meet current industry requirements but also future-proof your business.",
     icon: FaLightbulb,
   },
+  {
+    title: "Client-Centric Approach",
+    description:
+      "We believe that every project is unique. That’s why we take the time to understand your specific needs, challenges, and goals before crafting customized solutions. Your success is our priority, and we work closely with you at every stage to ensure seamless execution.",
+    icon: FaUsers,
+  },
+  {
+    title: "Innovative & Future-Ready Solutions",
+    description:
+      "Innovation is at the core of what we do. We continuously stay ahead of industry advancements, leveraging modern technologies and forward-thinking strategies to enhance safety, efficiency, and operational performance. Our solutions are designed to not only meet current industry requirements but also future-proof your business.",
+    icon: FaLightbulb,
+  },
 ];
 
 export const metadata = {
-  title: "Why Us | MK Engineering",
+  title: "Why Us | Indugrow",
   description: "",
 };
 
 const WhyUs = () => {
   return (
-    <div className="container mx-auto px-4 py-12">
+    <div className="container mx-auto px-4 pb-12">
       <div className=" relative bg-[url('https://i.ibb.co/Xfpkf4cZ/About-Us.webp')] w-full h-96 flex items-center justify-center">
         <div className="absolute w-full h-full bg-black/50 "></div>
         <div className="  flex items-center justify-center gap-10 z-20">
@@ -49,13 +61,13 @@ const WhyUs = () => {
           <ProfileHeading>Why Choose Us</ProfileHeading>
         </div>
       </div>
-      <div className="mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 gap-y-12 max-w-7xl mx-auto">
+      <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 lg:max-lg:gap-y-8 max-w-7xl mx-auto">
         {data.map((item, index) => (
           <div
             key={index}
-            className="relative bg-white border border-mk-orange p-6 rounded-lg shadow-md hover:shadow-lg group"
+            className="relative bg-white border border-primary-green p-6 rounded-lg shadow-md hover:shadow-lg group"
           >
-            <div className="absolute -top-7 left-7 bg-mk-orange text-white p-3 rounded-lg shadow-lg border group-hover:border-mk-orange group-hover:bg-white group-hover:text-mk-orange transition-all duration-1000">
+            <div className="absolute -top-7 left-6 md:max-md:left-4 group-hover:translate-x-64 sm:max-md:group-hover:translate-x-[900%] md:max-lg:group-hover:translate-x-60 lg:max-xl:group-hover:translate-x-48 lg:group-hover:translate-x-[500%] 2xl:group-hover:translate-x-72  bg-primary-green text-white p-3 rounded-lg shadow-lg border border-primary-green group-hover:border-primary-green group-hover:bg-white group-hover:text-primary-green transition-all duration-1000">
               <item.icon size={30} />
             </div>
             <h3 className="text-xl font-semibold text-gray-800 mt-8">
